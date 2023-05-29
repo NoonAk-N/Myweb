9a26f7637a3c2848ae6b1e647e8ce02b71bec6ae
NoonAK-N <noonakoud@gmail.com>
2023-05-29 09:42:02 +0200

    first

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import handlebars from 'vite-plugin-handlebars';
+import data from "./data.json" assert{type:'json'};
+import { defineConfig } from 'vite';
+export default defineConfig ({
+    base: './repo/',
+    build: {
+        rollupOptions: {
+            input : {
+                main: 'index.html',
+                bootstrap: 'index-bootstrap.html',
+                materialize: 'index-materialize.html',
+            }
+        },
+    },
+
+    plugins: [handlebars({
+      context : data
+        
+      
+    })],
+  });
